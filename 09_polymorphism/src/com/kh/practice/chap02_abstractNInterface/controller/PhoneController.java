2ebda7060238eb7b9bc130a3acc5c5d881e3c997
Seokkitdo <sjd12329@gmail.com>
2020-07-29 00:09:23 +0900

    스마트폰다형성

--- 09_polymorphism/src/com/kh/practice/chap02_abstractNInterface/controller/PhoneController.java
+++ 09_polymorphism/src/com/kh/practice/chap02_abstractNInterface/controller/PhoneController.java
@@ -1,12 +1,14 @@
 package com.kh.practice.chap02_abstractNInterface.controller;
 
 import com.kh.practice.chap02_abstractNInterface.model.vo.GalaxyNote9;
+import com.kh.practice.chap02_abstractNInterface.model.vo.Phone;
+import com.kh.practice.chap02_abstractNInterface.model.vo.V40;
 
 public class PhoneController {
 	private String[] result = new String[2];
-	
+	GalaxyNote9 G = new GalaxyNote9();
+	V40 V = new V40();
 	public String[] method() {
-		
 		return result;
 	}
 	
