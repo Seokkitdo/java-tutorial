121b25b306f002a3a11eb4cb1d37699ab7c43004
Seokkitdo <sjd12329@gmail.com>
2020-08-10 19:40:28 +0900

    상단바

--- 16_GUI/src/com/kh/example/chap02_layout/run/Run.java
+++ 16_GUI/src/com/kh/example/chap02_layout/run/Run.java
@@ -1,16 +1,16 @@
 package com.kh.example.chap02_layout.run;
 
-import com.kh.example.chap02_layout.view.F_PanelLayout;
+import com.kh.example.chap02_layout.view.C_GridLayout;
 
 public class Run {
 
 	public static void main(String[] args) {
 //		new A_BorderLayout();
 //		new B_FlowLayout();
-//		new C_GridLayout();
+		new C_GridLayout();
 //		new D_CardLayout();
 //		new E_NullLayout();
-		new F_PanelLayout();
+//		new F_PanelLayout();
 	}
 
 }
